cea88f0b818773aef7a411ae7e1ea2181babd16f
Solanar <and_555@hotmail.com>
2014-02-28 20:36:20 -0700

    create front app for views, add a homepage index

--- DisSoNet/DisSoNet/urls.py
+++ DisSoNet/DisSoNet/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
+
+from front import views as front_views
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'DisSoNet.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', front_views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- DisSoNet/run.sh
+++ DisSoNet/run.sh
@@ -1,3 +1,3 @@
 #start the server
 
-/usr/bin/python3.3 manage.py runserver
+/usr/bin/python3.3 manage.py runserver 0.0.0.0:8080
