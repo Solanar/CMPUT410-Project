8abca9c6b40e9b25297195e9eecb5d845b371e77
Solanar <and_555@hotmail.com>
2014-02-28 20:41:50 -0700

    home page url

--- DisSoNet/DisSoNet/urls.py
+++ DisSoNet/DisSoNet/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'DisSoNet.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', front_views.home, name='home'),
+    url(r'^$', front_views.home, name='home'), 
     url(r'^admin/', include(admin.site.urls)),
 )
