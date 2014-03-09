676e2d2da28348fee7b356822a8310ebd0181447
Tyler <tdavidso@ualberta.ca>
2014-03-09 17:03:26 -0600

    finalized friends tests and backend for friends/user1/user2

--- DisSoNet/front/tests/test_friends.py
+++ DisSoNet/front/tests/test_friends.py
@@ -12,13 +12,15 @@ class UsersAreFriendsViewTest(TestCase):
         password = 'test'
         User.objects.create_user('user1@test.ca', 'Test1', 'Man', password)
         User.objects.create_user('user2@test.ca', 'Test2', 'Man', password)
+        User.objects.create_user('user3@test.ca', 'Test3', 'Man', password)
 
         self.user1 = User.objects.get(email='user1@test.ca')
         self.user2 = User.objects.get(email='user2@test.ca')
+        self.user3 = User.objects.get(email='user3@test.ca')
 
         self.client = Client()
 
-    def test_are_friends_view(self):
+    def test_are_friends_view_no_friend_object(self):
         user_id_1 = self.user1.id
         user_id_2 = self.user2.id
         response = self.client.get(reverse('are_friends',
@@ -30,4 +32,37 @@ class UsersAreFriendsViewTest(TestCase):
         are_friends = response.context['are_friends']
         self.assertEqual(are_friends, 'NO',
                          'Should not be friends')
-        pass
+
+    def test_are_friends_view_not_friends(self):
+        Friends.objects.create(user_id_requester=self.user1,
+                               user_id_receiver=self.user2,
+                               accepted=False)
+
+        user_id_1 = self.user1.id
+        user_id_2 = self.user2.id
+        response = self.client.get(reverse('are_friends',
+                                           kwargs={'user_id_1': user_id_1,
+                                                   'user_id_2': user_id_2}))
+        query = response.context['query']
+        self.assertTrue(query == 'friends', 'Executed the wrong query')
+        friends = response.context['friends']
+        are_friends = response.context['are_friends']
+        self.assertEqual(are_friends, 'NO',
+                         'Friend Request Wrongly Accepted')
+
+    def test_are_friends_view_friends(self):
+        Friends.objects.create(user_id_requester=self.user2,
+                               user_id_receiver=self.user3,
+                               accepted=True)
+
+        user_id_2 = self.user2.id
+        user_id_3 = self.user3.id
+        response = self.client.get(reverse('are_friends',
+                                           kwargs={'user_id_1': user_id_2,
+                                                   'user_id_2': user_id_3}))
+        query = response.context['query']
+        self.assertTrue(query == 'friends', 'Executed the wrong query')
+        friends = response.context['friends']
+        are_friends = response.context['are_friends']
+        self.assertEqual(are_friends, 'YES',
+                         'The two should be friends')
--- DisSoNet/front/views/friends.py
+++ DisSoNet/front/views/friends.py
@@ -16,14 +16,14 @@ class AreFriends(BaseView):
         super(AreFriends, self).preprocess(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
+        user1 = User.objects.get(id=kwargs['user_id_1'])
+        user2 = User.objects.get(id=kwargs['user_id_2'])
         try:
-            user1 = User.objects.get(id=kwargs['user_id_1'])
-            user2 = User.objects.get(id=kwargs['user_id_2'])
-            friend1 = Friends.objects.get(user_id_requester=user1,
-                                          user_id_receiver=user2)
-            friend2 = Friends.objects.get(user_id_requester=user2,
-                                          user_id_receiver=user1)
-            if friend1.accepted or friend2.accepted:
+            friend1 = (Friends.objects.get(user_id_requester=user1,
+                                           user_id_receiver=user2) or
+                       Friends.objects.get(user_id_requester=user2,
+                                           user_id_receiver=user1))
+            if friend1.accepted:
                 self.context['are_friends'] = 'YES'
             else:
                 self.context['are_friends'] = 'NO'
