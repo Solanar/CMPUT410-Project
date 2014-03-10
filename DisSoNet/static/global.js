ed6e3eea9c20ef88ae8f04104fa2ec39af28caef
Olexiy Berjanskii <berjansk@cs.ualberta.ca>
2014-03-10 12:44:14 -0600

    Created interface for new Posts, added additional javascript functions that will be used for loading data from the server

--- DisSoNet/static/global.js
+++ DisSoNet/static/global.js
@@ -3,7 +3,8 @@ $(document).on('ready', function(){
 	$('.newPost').on('click', function(){$('#newPostModal').modal() });
 	$('.friendRequest .acceptFriend').on("click", function(){ processFriend($(this).parents('.friendRequest').data('friendid'), "accept") });
 	$('.friendRequest .rejectFriend').on("click", function(){ processFriend($(this).parents('.friendRequest').data('friendid'), "reject") });
-
+    $('#newPostForm .post_type').on("change", function(){console.log($(this).val()); $(this).val() == "image" ? $('#newPostForm .image_field').removeClass('hide') : $('#newPostForm .image_field').addClass('hide') });
+    $('#createPost').on('click', createPostGlobal);
 });
 
 
@@ -11,11 +12,25 @@ function getActivityStream(callback){
 	$.get("/author/posts", function(data){callback(data)}, "json");
 }
 
-function getUserPost(callback){
-	$.get("/author/posts", function(data){callback(data)}, "json");
+function getUserPost(callback, userid){
+	$.get("/author/"+userid+"/posts", function(data){callback(data)}, "json");
 }
 
-function sendPost(form){
+
+function processFriend(friendid, action){
+	alert('Processing Friend Request From: ' + friendid + " action: " + action)
+}
+
+function createPostGlobal(){
+    var data = $('#newPostForm').serialize();
+    /*TODO Massage form data here to meet server spec*/
+    alert("Posting the following to /posts \n" + data);
+    sendPost(data, console.log);
+    $('#newPostModal').modal("close");
+}
+
+
+function sendPost(data, callback){
     /*
     Fields from JSON example_artivle.json
     title: Title of the post
@@ -24,12 +39,7 @@ function sendPost(form){
     description: Brief description of the post, limited by characters of 100
     content-type: choice of [text/html, text/x-markdown, text/plain]
     content: Text field to input the contents of the post
-    author: Foreign key referenceing author	
-	*/
-	var data = form.serialize();
-
-}
-
-function processFriend(friendid, action){
-	alert('Processing Friend Request From: ' + friendid + " action: " + action)
+    author: Foreign key referenceing author 
+    */
+    $.post("/posts", data, callback, "json");
 }
