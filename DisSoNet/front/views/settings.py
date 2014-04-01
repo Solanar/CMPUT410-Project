0257c62005d480c380e5ace27d4c7c16847e5b09
geominion <geojbd@gmail.com>
2014-03-31 18:33:58 -0600

    Oops, I still had my login hardcoded

--- DisSoNet/front/views/settings.py
+++ DisSoNet/front/views/settings.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+import pycurl, json
 
-def contact(request):
+def initGithub(request):
     if request.method == 'POST': # If the form has been submitted...
         form = ContactForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            # ...
-            return HttpResponseRedirect('/thanks/') # Redirect after POST
+            github_url = "https://api.github.com/authorizations"
+            user_pwd = " : "
+            data = json.dumps({"scopes": ["repo"], "note": "getting-started"})
+            connection = pycurl.Curl()
+            connection.setopt(pycurl.URL, github_url)
+            connection.setopt(pycurl.USERPWD, user_pwd)
+            connection.setopt(pycurl.POST, 1)
+            connection.setopt(pycurl.POSTFIELDS, data)
+            connection.perform()
+            c.getinfo(pycurl.HTTP_CODE), c.getinfo(pycurl.EFFECTIVE_URL)
     else:
         form = ContactForm() # An unbound form
 
