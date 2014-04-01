e39375dd60ab3866ceee9b3f954a08afbfe6d026
geominion <geojbd@gmail.com>
2014-03-31 18:31:10 -0600

    Pycurl will be needed to run the settings once its implemented. I removed the remove icon from the views, and changed their sizes.

---
+++ DisSoNet/front/views/settings.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+def contact(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = ContactForm() # An unbound form
+
+    return render(request, 'contact.html', {
+        'form': form,
+    })
