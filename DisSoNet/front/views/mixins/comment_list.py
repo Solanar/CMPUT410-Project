b1faab374b347594e3a63de9ad3842dee866f9bb
Tyler <tdavidso@ualberta.ca>
2014-04-05 12:57:50 -0600

    Created CommentListMixin   - generates list of comments for a given post   - requires the post object be given in kwargs as ['post']
    Creates resource for posting a comment to a post

---
+++ DisSoNet/front/views/mixins/comment_list.py
@@ -0,0 +1,9 @@
+from data.models import Comment
+
+
+class CommentListMixin(object):
+
+    def preprocess(self, request, *args, **kwargs):
+        post = kwargs['post']
+        self.context['comments'] = Comment.objects.filter(post=post)
+        super(CommentListMixin, self).preprocess(request, *args, **kwargs)
