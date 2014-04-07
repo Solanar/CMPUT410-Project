a4964074aa6cdd6b2ba4a22cfb17f0088caab141
Solanar <and_555@hotmail.com>
2014-04-06 19:06:51 -0600

    code for searching our list of servers for the given author guid

--- DisSoNet/front/views/mixins/user.py
+++ DisSoNet/front/views/mixins/user.py
@@ -1,4 +1,6 @@
-from data.models import User
+from data.models import User  # , Server
+#import urllib2
+#import json
 
 
 class GetUserMixin(object):
@@ -10,11 +12,41 @@ class GetUserMixin(object):
                 user = User.objects.get(guid=kwargs['user_filter']['user_id'])
             except:
                 pass
+            """
+            if not user and not kwargs['user_filter']['remote']:
+                user = self.getRemoteUser(kwargs['user_filter']['user_id'])
+            """
         elif request.user.is_authenticated():
-            print ("hi2")
             user = User.objects.get(email=request.user.email)
         self.context['user_obj'] = user
         super(GetUserMixin, self).preprocess(request, *args, **kwargs)
+
+    """
+    def getRemoteUser(self, guid):
+        for server in Server.objects.all():
+            url = server.url[:-1]  # django adds trailing /
+            url = url + ':' + str(server.port) + '/'
+            url = url + "author/" + guid + "/"
+            headers = {'Accept': 'application/json'}
+            req = urllib2.Request(url, None, headers)
+            try:
+                data = urllib2.urlopen(req)
+                jsonData = json.load(data)
+                print ("hi", jsonData)
+                jsonguid = jsonData['author'][0]['id']
+                jsondisplayName = jsonData['author'][0]['displayname']
+                user = User(guid=jsonguid,
+                            firstName=jsondisplayName)
+                print ("hi1", user)
+                return user
+            except urllib2.URLError as e:
+                msg = ""
+                if hasattr(e, 'reason'):
+                    msg = "Reason: " + str(e.reason)
+                if hasattr(e, 'code'):
+                    msg = msg + " Error code: " + str(e.code)
+                print ("Could not connect to: " + url + " because: " + msg)
+    """
 
 
 class GetAllUsersMixin(object):
