7a3c1c6de6623a673164356331b5671a51b2c853
Solanar <and_555@hotmail.com>
2014-04-06 19:27:34 -0600

    josh/i moved the remote stuff to another file

--- DisSoNet/front/views/post.py
+++ DisSoNet/front/views/post.py
@@ -1,13 +1,10 @@
 from .base import BaseView
-from data.forms import PostCreationForm
 from data.models import Post, Comment, User
-from django.db.models import Q
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
 from .mixins.friends_list import FriendsListMixin
 from .mixins.post_list import PostListMixin
 from .mixins.comment_list import CommentListMixin
-from .author import processRequestFromOtherServer, getAuthorDict
+from .author import processRequestFromOtherServer
 
 
 # http://service/posts (all posts marked as public on the server)
@@ -23,7 +20,8 @@ class PublicPosts(PostListMixin, BaseView):
     def get(self, request, *args, **kwargs):
         if request.META.get('HTTP_ACCEPT') == 'application/json':
             # This a json request from another server
-            processRequestFromOtherServer(self.context['post_list'])
+            return processRequestFromOtherServer(self.context['post_list'],
+                                                 "posts")
         else:
             # Serve django objects
             return self.render_to_response(self.context)
@@ -63,7 +61,8 @@ class PostResource(PostListMixin, BaseView):
     def get(self, request, *args, **kwargs):
         if request.META.get('HTTP_ACCEPT') == 'application/json':
             # This a json request from another server
-            processRequestFromOtherServer(self.context["post_list"], "posts")
+            return processRequestFromOtherServer(self.context["post_list"],
+                                                 "posts")
         else:
             # Serve django objects
             self.context["post"] = self.context["post_list"]
@@ -93,7 +92,8 @@ class AuthorStream(FriendsListMixin, PostListMixin, BaseView):
     def get(self, request, *args, **kwargs):
         if request.META.get('HTTP_ACCEPT') == 'application/json':
             # This a json request from another server
-            processRequestFromOtherServer(self.context['post_list'], "posts")
+            return processRequestFromOtherServer(self.context['post_list'],
+                                                 "posts")
         else:
             # Serve django objects
             return self.render_to_response(self.context)
@@ -114,7 +114,8 @@ class VisiblePostToUser(FriendsListMixin, PostListMixin, BaseView):
     def get(self, request, *args, **kwargs):
         if request.META.get('HTTP_ACCEPT') == 'application/json':
         # This a json request from another server
-            processRequestFromOtherServer(self.context['post_list'], "posts")
+            return processRequestFromOtherServer(self.context['post_list'],
+                                                 "posts")
         else:
             return self.render_to_response(self.context)
 
@@ -144,86 +145,3 @@ class PostComments(CommentListMixin, BaseView):
                                          content=post_content)
         comment.save()
         return HttpResponseRedirect(request.path)
-
-
-def getPostDict(post_object):
-    """ From all post URLS should return a list of posts like the following.
-
-    Of the form:
-    { "posts":[{"title":"string",
-                "source":"url",
-                "origin":"url",
-                "description":"string",
-                "content-type":"text/*",
-                "content":"string",
-                "author":{"id":"sha1",
-                          "host":"host",
-                          "displayname":"name",
-                          "url":"url_to_author"},
-                "categories":["cat1", "cat2"],
-                "comments":[{"author":{"id":"sha1",
-                                       "host":"url",
-                                       "displayname":"name"},
-                             "comment":"string",
-                             "pubDate":"date",
-                             "guid":"sha1"}]
-                "pubdate":"date",
-                "guid":"sha1",
-                "visibility":"PUBLIC"}]}
-
-    This function will return the representation of a post to go into this list
-
-    """
-    post_dict = {}
-    post_dict["title"] = post_object.title
-    post_dict["source"] = post_object.source
-    post_dict["origin"] = post_object.origin
-    post_dict["description"] = post_object.description
-    post_dict["content-type"] = post_object.content_type
-    post_dict["content"] = post_object.content
-    # TODO python datetime is not JSON serializable
-    timestring = post_object.published_date.strftime("%a %b %d %h:%m:%s mst %y")
-    post_dict["pubdate"] = timestring
-    # post_dict["pubdate"] = post_object.published_date
-    post_dict["guid"] = post_object.guid
-    post_dict["visibility"] = post_object.visibility
-
-    # get the post author, convert to dict and add to post_dict
-    author_dict = getAuthorDict(post_object.author, include_url=True)
-    post_dict["author"] = author_dict
-
-    # get all comments on this post of return them
-    comment_list = Comment.objects.filter(post=post_object)
-    comment_dict_list = getCommentDictList(comment_list)
-    post_dict["comments"] = comment_dict_list
-
-    return post_dict
-
-
-def getCommentDictList(comment_list):
-    """ Take a list of comment objects, returns list of dict representations.
-
-    Of the form:
-    "comments":[{"author":{"id":"sha1",
-                           "host":"url",
-                           "displayname":"name"},
-                 "comment":"string",
-                 "pubDate":"date",
-                 "guid":"sha1"}]
-
-    :returns: A list of dicts
-
-    """
-    comment_dict_list = []
-    for comment in comment_list:
-        comment_dict = {}
-        comment_dict["author"] = getAuthorDict(comment.user)
-        comment_dict["comment"] = comment.content
-        # TODO python datetime is not JSON serializable
-        timestring = comment.published_date.strftime("%a %b %d %h:%m:%s mst %y")
-        comment_dict["pubDate"] = timestring
-        comment_dict["guid"] = comment.guid
-        comment_dict_list.append(comment_dict)
-
-    return comment_dict_list
-
